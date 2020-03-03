b4a56925fea6499791043805875d1d2bd9a615c3
Anand Chowdhary <mail@anandchowdhary.com>
2020-03-03 14:32:59 +0100

    :sparkles: Show camera

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text, StatusBar} from 'react-native';
+import {SafeAreaView, StyleSheet, Text, StatusBar, View} from 'react-native';
 import Camera from './components/Camera';
 
 declare var global: {HermesInternal: null | {}};
@@ -9,13 +9,20 @@ const App = () => {
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <Camera />
-        <Text>Hello</Text>
+        <View style={styles.container}>
+          <Text>Cam:</Text>
+          <Camera />
+          <Text>Hello</Text>
+        </View>
       </SafeAreaView>
     </>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'gray',
+  },
+});
 
 export default App;
--- components/Camera.tsx
+++ components/Camera.tsx
@@ -22,57 +22,58 @@ export default () => {
     console.log(data.uri);
   };
   return (
-    <RNCamera
-      style={styles.preview}
-      type={RNCamera.Constants.Type.back}
-      flashMode={RNCamera.Constants.FlashMode.on}
-      androidCameraPermissionOptions={{
-        title: 'Permission to use camera',
-        message: 'We need your permission to use your camera',
-        buttonPositive: 'Ok',
-        buttonNegative: 'Cancel',
-      }}
-      androidRecordAudioPermissionOptions={{
-        title: 'Permission to use audio recording',
-        message: 'We need your permission to use your audio',
-        buttonPositive: 'Ok',
-        buttonNegative: 'Cancel',
-      }}>
-      {({camera, status, recordAudioPermissionStatus}) => {
-        if (status !== 'READY') return <PendingView />;
-        return (
-          <View
-            style={{flex: 0, flexDirection: 'row', justifyContent: 'center'}}>
-            <TouchableOpacity
-              onPress={() => takePicture(camera)}
-              style={styles.capture}>
-              <Text style={{fontSize: 14}}> SNAP </Text>
-            </TouchableOpacity>
-          </View>
-        );
-      }}
-    </RNCamera>
+    <>
+      <RNCamera
+        style={styles.preview}
+        type={RNCamera.Constants.Type.back}
+        flashMode={RNCamera.Constants.FlashMode.on}
+        androidCameraPermissionOptions={{
+          title: 'Permission to use camera',
+          message: 'We need your permission to use your camera',
+          buttonPositive: 'Ok',
+          buttonNegative: 'Cancel',
+        }}
+        androidRecordAudioPermissionOptions={{
+          title: 'Permission to use audio recording',
+          message: 'We need your permission to use your audio',
+          buttonPositive: 'Ok',
+          buttonNegative: 'Cancel',
+        }}>
+        {({camera, status, recordAudioPermissionStatus}) => {
+          if (status !== 'READY') return <PendingView />;
+          return (
+            <View style={styles.container}>
+              <TouchableOpacity
+                onPress={() => takePicture(camera)}
+                style={styles.capture}>
+                <Text style={{fontSize: 14}}> SNAP </Text>
+              </TouchableOpacity>
+            </View>
+          );
+        }}
+      </RNCamera>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    flexDirection: 'column',
-    backgroundColor: 'black',
+    //   flex: 1,
+    //   flexDirection: 'column',
+    //   backgroundColor: 'black',
   },
   preview: {
-    flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
+    //   flex: 1,
+    //   justifyContent: 'flex-end',
+    //   alignItems: 'center',
   },
   capture: {
-    flex: 0,
-    backgroundColor: '#fff',
-    borderRadius: 5,
-    padding: 15,
-    paddingHorizontal: 20,
-    alignSelf: 'center',
-    margin: 20,
+    //   flex: 0,
+    //   backgroundColor: '#fff',
+    //   borderRadius: 5,
+    //   padding: 15,
+    //   paddingHorizontal: 20,
+    //   alignSelf: 'center',
+    //   margin: 20,
   },
 });
