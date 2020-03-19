0a47bf037723a932aee62a45a9db19be480df272
Anand Chowdhary <mail@anandchowdhary.com>
2020-03-19 23:08:40 +0530

    :sparkles: Upload image to Firebase

--- components/Camera.tsx
+++ components/Camera.tsx
@@ -3,6 +3,47 @@ import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import {RNCamera} from 'react-native-camera';
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 import {faCamera} from '@fortawesome/free-solid-svg-icons';
+import * as firebase from 'firebase/app';
+import 'firebase/database';
+import 'firebase/storage';
+
+/**
+ * Converts a base64 image to Blob
+ * @source https://github.com/react-native-community/react-native-image-picker/issues/510#issuecomment-382359022
+ * @param url - Base 64 encoded image
+ */
+function urlToBlob(url: string): Promise<Blob> {
+  return new Promise((resolve, reject) => {
+    var xhr = new XMLHttpRequest();
+    xhr.onerror = reject;
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === 4) {
+        resolve(xhr.response);
+      }
+    };
+    xhr.open('GET', url);
+    xhr.responseType = 'blob';
+    xhr.send();
+  });
+}
+
+/**
+ * This is the public web API key (you can see this)
+ * This is NOT the Firebase admin API key
+ */
+const WEB_API_KEY = 'AIzaSyCSo4PAiC-cr1euluE4XybGZntQve3rc78';
+
+try {
+  firebase.initializeApp({
+    apiKey: WEB_API_KEY,
+    authDomain: 'frankenstein-ce9dd.firebaseapp.com',
+    databaseURL: 'https://frankenstein-ce9dd.firebaseio.com',
+    projectId: 'frankenstein-ce9dd',
+    storageBucket: 'frankenstein-ce9dd.appspot.com',
+    messagingSenderId: '168976872219',
+    appId: '1:168976872219:web:bd6ef874bb4a27feb419be',
+  });
+} catch (error) {}
 
 const PendingView = () => (
   <View
@@ -18,10 +59,25 @@ const PendingView = () => (
 
 export default () => {
   const takePicture = async (camera: RNCamera) => {
-    const options = {quality: 0.5, base64: true};
-    const data = await camera.takePictureAsync(options);
-    //  eslint-disable-next-line
-    console.log(data.uri);
+    const data = await camera.takePictureAsync({
+      quality: 0.5,
+      base64: true,
+      width: 300,
+    });
+    if (!data.base64) return;
+    const blob = await urlToBlob(data.uri);
+    const ref = (
+      await firebase
+        .storage()
+        .ref(new Date().toISOString() + '.jpg')
+        .put(blob, {contentType: 'image/jpeg'})
+    ).ref;
+    const url = await ref.getDownloadURL();
+    if (!url) return console.log('No download URL');
+    await firebase
+      .database()
+      .ref()
+      .update({image: url});
   };
   return (
     <View style={styles.container}>
