c8da6432d293b8809bfa78d3d8e10ab7d5e72750
Anand Chowdhary <mail@anandchowdhary.com>
2020-03-19 22:10:30 +0530

    :heavy_plus_sign: Add firebase

--- web/app.ts
+++ web/app.ts
@@ -1,1 +1,5 @@
-console.log('OK');
+/**
+ * This is the public web API key (you can see this)
+ * This is NOT the Firebase admin API key
+ */
+const WEB_API_KEY = 'AIzaSyCSo4PAiC-cr1euluE4XybGZntQve3rc78';
