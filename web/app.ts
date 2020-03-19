f518a776f37edab84247fc982f10a039f9fde024
Anand Chowdhary <mail@anandchowdhary.com>
2020-03-19 22:16:03 +0530

    :wrench: Add dom to libraries

--- web/app.ts
+++ web/app.ts
@@ -1,5 +1,26 @@
+import * as firebase from 'firebase/app';
+import 'firebase/database';
+import 'firebase/storage';
+
 /**
  * This is the public web API key (you can see this)
  * This is NOT the Firebase admin API key
  */
 const WEB_API_KEY = 'AIzaSyCSo4PAiC-cr1euluE4XybGZntQve3rc78';
+
+firebase.initializeApp({
+  apiKey: WEB_API_KEY,
+  authDomain: 'frankenstein-ce9dd.firebaseapp.com',
+  databaseURL: 'https://frankenstein-ce9dd.firebaseio.com',
+  projectId: 'frankenstein-ce9dd',
+  storageBucket: 'frankenstein-ce9dd.appspot.com',
+  messagingSenderId: '168976872219',
+  appId: '1:168976872219:web:bd6ef874bb4a27feb419be',
+});
+
+// const store = storage();
+const db = firebase.database();
+
+db.ref('image').on('value', value => {
+  console.log(value.val());
+});
