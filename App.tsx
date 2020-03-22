da9bd68c305b81f15841b2da42fe0a0dbb36b6ec
Anand Chowdhary <mail@anandchowdhary.com>
2020-03-22 20:04:40 +0530

    :tada: Initialize new Expo app

--- App.tsx
+++ App.tsx
@@ -1,51 +1,19 @@
-import React, {useState} from 'react';
-import {SafeAreaView, StyleSheet, Text, StatusBar, View} from 'react-native';
-import Navbar from './components/Navbar';
-import Camera from './components/Camera';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-declare var global: {HermesInternal: null | {}};
-
-const App = () => {
-  const [active, setActive] = useState('Photo');
+export default function App() {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <View style={styles.container}>
-          <View style={styles.header}>
-            <Text style={styles.title}>Mockingbird</Text>
-          </View>
-          <View style={styles.camera}>
-            {active === 'Settings' ? <Text>Settings</Text> : <></>}
-            {active === 'Subtitles' ? <Text>Subtitles</Text> : <></>}
-            {active === 'Photo' ? <Camera live={false} /> : <></>}
-            {active === 'Live' ? <Camera live={true} /> : <></>}
-          </View>
-          <Navbar active={active} setActive={setActive} />
-        </View>
-      </SafeAreaView>
-    </>
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+    </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'gray',
-    height: '100%',
-  },
-  camera: {
     flex: 1,
-  },
-  header: {
     backgroundColor: '#fff',
-    display: 'flex',
-    flexDirection: 'row',
+    alignItems: 'center',
     justifyContent: 'center',
   },
-  title: {
-    fontSize: 20,
-    padding: 15,
-  },
 });
-
-export default App;
