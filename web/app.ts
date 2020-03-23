49a65c5aa3edc721243379195f054a154330d8d1
Anand Chowdhary <mail@anandchowdhary.com>
2020-03-23 15:02:30 +0530

    :sparkles: Use scoped images

--- web/app.ts
+++ web/app.ts
@@ -1,32 +1,34 @@
-import * as firebase from 'firebase/app';
-import 'firebase/database';
-import 'firebase/storage';
+import * as firebase from "firebase/app";
+import "firebase/database";
+import "firebase/storage";
 
 /**
  * This is the public web API key (you can see this)
  * This is NOT the Firebase admin API key
  */
-const WEB_API_KEY = 'AIzaSyCSo4PAiC-cr1euluE4XybGZntQve3rc78';
+const WEB_API_KEY = "AIzaSyCSo4PAiC-cr1euluE4XybGZntQve3rc78";
 
 firebase.initializeApp({
   apiKey: WEB_API_KEY,
-  authDomain: 'frankenstein-ce9dd.firebaseapp.com',
-  databaseURL: 'https://frankenstein-ce9dd.firebaseio.com',
-  projectId: 'frankenstein-ce9dd',
-  storageBucket: 'frankenstein-ce9dd.appspot.com',
-  messagingSenderId: '168976872219',
-  appId: '1:168976872219:web:bd6ef874bb4a27feb419be',
+  authDomain: "frankenstein-ce9dd.firebaseapp.com",
+  databaseURL: "https://frankenstein-ce9dd.firebaseio.com",
+  projectId: "frankenstein-ce9dd",
+  storageBucket: "frankenstein-ce9dd.appspot.com",
+  messagingSenderId: "168976872219",
+  appId: "1:168976872219:web:bd6ef874bb4a27feb419be"
 });
 
 // const store = storage();
 const db = firebase.database();
 
-db.ref('image').on('value', snapshot => {
+const slug = window.location.pathname;
+
+db.ref(slug).on("value", snapshot => {
   const value = snapshot.val();
   const loader = new Image();
-  loader.src = value;
-  loader.addEventListener('load', () => {
-    const img = document.querySelector<HTMLDivElement>('.image');
-    if (img) img.style.backgroundImage = `url(${value})`;
+  loader.src = value.url;
+  loader.addEventListener("load", () => {
+    const img = document.querySelector<HTMLDivElement>(".image");
+    if (img) img.style.backgroundImage = `url(${value.url})`;
   });
 });
